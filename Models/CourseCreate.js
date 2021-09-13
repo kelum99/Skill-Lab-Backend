2c8c85aafad1ae0b16fec981c6b91404f60a9ff4
NemanthiEdirisooriya <nemanthidasu23@gmail.com>
2021-09-13 21:07:43 +0530

    course

---
+++ Models/CourseCreate.js
@@ -0,0 +1,45 @@
+
+//const { ObjectId } = mongoose.Schema;
+
+const mongoose = require('mongoose');
+
+const coursecreateSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      minlength: 3,
+      maxlength: 320,
+      required: true,
+    },
+    
+    description: {
+      type: {},
+      minlength: 200,
+      required: true,
+    },
+    price: {
+      type: Number,
+      default: 0.00,
+    },
+  
+    paid: {
+      type: Boolean,
+      default: false,
+    },
+
+    category:{
+      type: String,
+      trim: true,
+      minlength: 3,
+      maxlength: 320,
+      required: true,
+    },
+
+    
+  });
+
+  
+
+const CourseCreate = mongoose.model('CourseCreate', coursecreateSchema);
+module.exports = CourseCreate ;
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
 
-app.use(express.json());
+app.use(express.json({limit:"5mb"}));
 app.use(cors());
 
 mongoose.connect(process.env.DB_CONNECT, err => {
@@ -19,6 +19,16 @@ mongoose.connect(process.env.DB_CONNECT, err => {
 
 const routWallet = require("./Routes/FinanceRoute");
 app.use("/api/finance", routWallet);
+
+/*
+const image_upload = require("./Routes/courseRoute");
+app.use("/api/course",image_upload);*/
+
+const coursecreate = require("./Routes/courseRoute");
+app.use("/api/course",coursecreate);
+
+
+
 
 app.listen(4000, err => {
   if (!err) {
